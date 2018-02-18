7fad03437a9db3b27b8902970e1d85fcdb8b4258
YaroslavBoiko <35149052+YaroslavBoiko@users.noreply.github.com>
2018-02-18 23:39:58 +0200

    Create hidden.py

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "exIdOwfV8yiQKpYzj5P71MeyC",
+            "consumer_secret": "kRf64kVlcRROj4D6okGm6OEJk9lPyimxDYAuQKZzJy4yFskOiJ",
+            "token_key": "4909063403-ouA1Kx9q9wYCqtSHrICBxXIbyhcF2zR6odayAWg",
+            "token_secret": "KW24xdlum3aCK8Y4oHje8drrGI4AElzc42ma2roUFhDSS"}
